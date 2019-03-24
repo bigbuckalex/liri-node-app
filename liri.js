126a7bfc836f208f1587b9ccd968263c5972f8b3
bigbuckalex <bigbuckalex@gmail.com>
2019-03-24 18:47:47 -0400

    movie-this done

--- liri.js
+++ liri.js
@@ -7,22 +7,42 @@ const moment = require("moment");
 let command = process.argv[2];
 let query = process.argv.slice(3).join("%20");
 
-console.log(command);
-
-console.log(query);
 
 if(command === "concert-this") concertThis();
+if(command === "movie-this") movieThis();
 
 
 function concertThis(){
     axios.get("https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp")
         .then((response)=>{
-            console.log(response.data);
+            console.log("==================================================");
             for(let i=0; i<response.data.length; i++){
                 console.log(response.data[i].venue.name);
                 console.log(response.data[i].venue.city + ", " + response.data[i].venue.country);
                 let date = response.data[i].datetime.substring(0,10);
                 console.log(moment(date, "YYYY-MM-DD").format("MM/DD/YYYY") + "\n");
             }
+            console.log("==================================================");
+        })
+}
+
+function spotifyThis(){
+    
+}
+
+function movieThis(){
+    axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + query)
+        .then((response)=>{
+            console.log(response.data);
+            console.log("==================================================");
+            console.log(response.data.Title);
+            console.log("Released: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Cast: " + response.data.Actors);
+            console.log("==================================================");
         })
 }
